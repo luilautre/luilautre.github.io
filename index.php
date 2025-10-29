56e8ffeee8ffb1049a9274408765030c60b10346
luilautre <achillebou13@gmail.com>
2025-10-29 09:52:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <span id="wts2195503"></span>
+  <script>var wts=document.createElement('script');wts.async=true;
+  wts.src='https://app.ardalio.com/wts7.js';document.head.appendChild(wts);
+  wts.onload = function(){ wtsl7(2195503,18); };
+  </script><noscript><img src="https://app.ardalio.com/7/18/2195503.png"></noscript>
+  <meta charset="utf-8">
+  <title>luilautre | messagerie gratuite et plusieurs jeux</title>
+  <meta name="description" content="Bienvenue chez luilautre vous retrouverez des jeux-vidéos comme dessine ton jeu une messagerie gratuite et bien d'autres"/>
+  <meta name="keywords" content="jeux-vidéos, dessine ton jeu, messagerie gratuite" />
+  <script>
+    (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "tankwn4e84");
+  </script>
+  <meta name="google-site-verification" content="a4bNKJnqP5jE35HIOj76BRI37Bf-ibiU4Ek9nKkpwsk" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="favicon.png" type="image/x-icon">
+</head>
+<body>
+    <h1>Bienvenue chez luilautre !</h1>
+    <div id="buttons">
+        <a href="/message.html"><p>messages</p></a>
+        <a href="/share"><p>fichiers</p></a>
+        <a href="/games"><p>Jeux</p></a>
+        <a href="/scratch-site"><p>luilautre_code</p></a>
+    </div>
+    <button id="theme-toggle" aria-label="Changer de thème">🌙</button>
+    <p>par : Achille Boulanger</p>
+    <script src="script.js"><a href="/comments.html">bug</a></script>
+</body>
+</html>
